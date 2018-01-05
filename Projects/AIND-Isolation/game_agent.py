7da4efaa101039534f32ee2b7fbbc1da34c582d1
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2018-01-05 03:47:35 -0500

    3/4 tests passing for Depth-Limited Minimax Search

--- Projects/AIND-Isolation/game_agent.py
+++ Projects/AIND-Isolation/game_agent.py
@@ -213,8 +213,64 @@ class MinimaxPlayer(IsolationPlayer):
             raise SearchTimeout()
 
         # TODO: finish this function!
-        raise NotImplementedError
+        return self._helper_minimax(game=game, depth=depth) # Returning the best legal move
 
+    def _helper_minimax(self, game, depth):
+
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        # Copied from AIMA Pseudocode
+        def terminal_test(game, depth): # Either the depth is Zero(node is terminal node) OR there are no legal moves
+            if self.time_left() < self.TIMER_THRESHOLD:
+                raise SearchTimeout()
+
+            legal_moves = game.get_legal_moves(self)
+            return depth == 0 or len(legal_moves) == 0
+
+        def min_value(game, depth):
+
+            if self.time_left() < self.TIMER_THRESHOLD:
+                raise SearchTimeout()
+
+            if terminal_test(game, depth):
+                return self.score(game, self)
+
+            bestScore = float("inf")
+
+            for move in game.get_legal_moves(self):
+                bestScore = min(bestScore, max_value(game.forecast_move(move), depth - 1)) # Get the bestScore
+
+            return bestScore
+
+        def max_value(game, depth):
+
+            if self.time_left() < self.TIMER_THRESHOLD:
+                raise SearchTimeout()
+
+            if terminal_test(game, depth):
+                return self.score(game, self)
+
+            bestScore = float("-inf")
+
+            for move in game.get_legal_moves(self):
+                bestScore = max(bestScore, min_value(game.forecast_move(move), depth - 1)) # Get the bestScore
+
+            return bestScore
+
+        legal_moves = game.get_legal_moves(self)
+
+        if not legal_moves: # If there are no more legal moves then return (-1, -1)
+            return (-1, -1)
+
+        bestMove = legal_moves[0] # Assuming first legal move as the best move
+        bestScore = float("-inf")
+
+        for move in legal_moves:
+            nextMove = game.forecast_move(move)
+            bestMove, bestScore = max([(bestMove, bestScore), (move, min_value(nextMove, depth - 1))] , key=lambda item: item[1]) # Get the move that has the bestScore
+
+        return bestMove
 
 class AlphaBetaPlayer(IsolationPlayer):
     """Game-playing agent that chooses a move using iterative deepening minimax
